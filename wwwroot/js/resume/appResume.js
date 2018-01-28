cfb5388423ed556b883b4c4380e6a067712aa250
Anshul Dubey <anshuldubey@users.noreply.github.com>
2018-01-28 12:16:34 +0530

    Add files via upload

---
+++ wwwroot/js/resume/appResume.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    //'use strict';
+
+    angular.module('appResume', [
+        // Angular modules 
+        'ngRoute'
+
+        // Custom modules 
+
+        // 3rd Party Modules
+
+    ]);
+})();
---
+++ wwwroot/js/resume/controllerResume.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    //'use strict';
+    angular
+        .module('appResume', [])
+        .controller('controllerResume', controller);
+
+    controller.$inject = ['$scope'];
+
+    function controller($scope) {
+        $scope.title = 'controller';
+
+        activate();
+
+        function activate() { }
+    }
+})();
